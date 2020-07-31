991b03e3c78b64779ec07b213e65e06165a6ff12
GaruGaru <garuglieritommaso.dev@gmail.com>
2020-07-31 11:50:15 +0200

    added support for instance fleet clusters

--- aws/cluster_provider.go
+++ aws/cluster_provider.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/emr"
 	"github.com/patrickmn/go-cache"
 	"strings"
@@ -12,8 +13,9 @@ import (
 )
 
 type ClusterProvider struct {
-	client *emr.EMR
-	cache  *cache.Cache
+	emrClient *emr.EMR
+	ec2Client *ec2.EC2
+	cache     *cache.Cache
 }
 
 func NewClusterProvider() *ClusterProvider {
@@ -22,8 +24,9 @@ func NewClusterProvider() *ClusterProvider {
 	}))
 
 	return &ClusterProvider{
-		client: emr.New(sess),
-		cache:  cache.New(60*time.Minute, 24*time.Hour),
+		emrClient: emr.New(sess),
+		ec2Client: ec2.New(sess),
+		cache:     cache.New(60*time.Minute, 24*time.Hour),
 	}
 }
 
@@ -61,23 +64,23 @@ func (c *ClusterProvider) listTargetMasters(ctx context.Context) (map[string]str
 			return nil, err
 		}
 
-		clusterWithMaster[*cluster.Name] = fmt.Sprintf("%s:8889", master)
+		clusterWithMaster[*cluster.Cluster.Name] = fmt.Sprintf("%s:8889", master)
 	}
 
 	return clusterWithMaster, nil
 }
 
-func (c *ClusterProvider) listTargetClusters(ctx context.Context) ([]*emr.ClusterSummary, error) {
+func (c *ClusterProvider) listTargetClusters(ctx context.Context) ([]*emr.DescribeClusterOutput, error) {
 	req := &emr.ListClustersInput{
 		ClusterStates: aws.StringSlice([]string{"WAITING"}),
 	}
 
-	clusters := make([]*emr.ClusterSummary, 0)
-	err := c.client.ListClustersPagesWithContext(ctx, req, func(output *emr.ListClustersOutput, b bool) bool {
+	clusters := make([]*emr.DescribeClusterOutput, 0)
+	err := c.emrClient.ListClustersPagesWithContext(ctx, req, func(output *emr.ListClustersOutput, b bool) bool {
 
 		for _, cluster := range output.Clusters {
 
-			descr, _ := c.client.DescribeCluster(&emr.DescribeClusterInput{
+			descr, _ := c.emrClient.DescribeCluster(&emr.DescribeClusterInput{
 				ClusterId: cluster.Id,
 			})
 
@@ -85,7 +88,7 @@ func (c *ClusterProvider) listTargetClusters(ctx context.Context) ([]*emr.Cluste
 				continue
 			}
 
-			clusters = append(clusters, output.Clusters...)
+			clusters = append(clusters, descr)
 
 		}
 		return true
@@ -94,22 +97,52 @@ func (c *ClusterProvider) listTargetClusters(ctx context.Context) ([]*emr.Cluste
 	return clusters, err
 }
 
-func (c *ClusterProvider) getClusterMasterInstance(cluster *emr.ClusterSummary) (string, error) {
-	instanceGroups, err := c.client.ListInstanceGroups(&emr.ListInstanceGroupsInput{
-		ClusterId: cluster.Id,
+func (c *ClusterProvider) getClusterMasterInstance(cluster *emr.DescribeClusterOutput) (string, error) {
+
+	instanceCollectionType := cluster.Cluster.InstanceCollectionType
+
+	if *instanceCollectionType == emr.InstanceCollectionTypeInstanceGroup {
+		return c.getMasterInstanceForNodeGroup(cluster)
+	} else if *instanceCollectionType == emr.InstanceCollectionTypeInstanceFleet {
+		return c.getMasterInstanceForFleet(cluster)
+	}
+
+	return "", fmt.Errorf("unrecognized instance type %s", *instanceCollectionType)
+}
+
+func (c *ClusterProvider) getMasterInstanceForFleet(cluster *emr.DescribeClusterOutput) (string, error) {
+
+	instances, err := c.emrClient.ListInstances(&emr.ListInstancesInput{
+		ClusterId:         cluster.Cluster.Id,
+		InstanceFleetType: aws.String(emr.InstanceFleetTypeMaster),
 	})
 
 	if err != nil {
 		return "", err
 	}
 
-	for _, group := range instanceGroups.InstanceGroups {
-		if *group.InstanceGroupType != emr.InstanceGroupTypeMaster {
-			continue
-		}
+	if len(instances.Instances) == 0 {
+		return "", fmt.Errorf("no master instance found for cluster %s", *cluster.Cluster.Id)
+	}
 
-		instances, err := c.client.ListInstances(&emr.ListInstancesInput{
-			ClusterId:       cluster.Id,
+	return *instances.Instances[0].PrivateIpAddress, nil
+}
+
+func (c *ClusterProvider) getMasterInstanceForNodeGroup(cluster *emr.DescribeClusterOutput) (string, error) {
+
+	instanceGroups, err := c.emrClient.ListInstances(&emr.ListInstancesInput{
+		ClusterId:          cluster.Cluster.Id,
+		InstanceGroupTypes: []*string{aws.String(emr.InstanceGroupTypeMaster)},
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	for _, group := range instanceGroups.Instances {
+
+		instances, err := c.emrClient.ListInstances(&emr.ListInstancesInput{
+			ClusterId:       cluster.Cluster.Id,
 			InstanceGroupId: group.Id,
 		})
 
@@ -118,13 +151,13 @@ func (c *ClusterProvider) getClusterMasterInstance(cluster *emr.ClusterSummary) 
 		}
 
 		if len(instances.Instances) == 0 {
-			return "", fmt.Errorf("no master instances found for cluster %s", *cluster.Id)
+			return "", fmt.Errorf("no master instances found for cluster %s", *cluster.Cluster.Id)
 		}
 
 		return *instances.Instances[0].PrivateIpAddress, nil
 	}
 
-	return "", fmt.Errorf("no master intance found for cluster: %s", *cluster.Id)
+	return "", fmt.Errorf("no master instance found for cluster %s", *cluster.Cluster.Id)
 }
 
 func hasPrestoInstalled(descr *emr.DescribeClusterOutput) bool {
